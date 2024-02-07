d288b524a09e60f84c1476a0f1598950911ec49b
Khanh <nguyenkhanhan79@gmail.com>
2024-02-07 20:52:08 +0700

    fix (HorizontalBar) : Swiper pagination not working

--- Lazada/obj/Debug/net8.0/Lazada.AssemblyInfo.cs
+++ Lazada/obj/Debug/net8.0/Lazada.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lazada")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af991ba0b2bc2a2ca0bfd2574f9a0c673f0109cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lazada")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lazada")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
